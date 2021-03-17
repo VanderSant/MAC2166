4859a2d9a2158a536b89f4423aa71c0720fe8cf6
VanderSant <vanderson.santos@usp.br>
2021-03-17 04:19:04 -0300

    :art: fix a structure

--- ep1/EP1.c
+++ ep1/EP1.c
@@ -34,33 +34,40 @@ int main()
     scanf("%d", &Npalpites);
     printf("Entre com a senha de %d digitos (0 para valor aleatorio): ", k);
     scanf("%d",&senha);
+
     if (senha == 0) {
       srand(time(NULL));
-      for (i=0; i<k; i++)
-        senha = senha*10 + (rand() % c + 1);}
-        CNpalpites= Npalpites;
+      for (i=0; i<k; i++){
+        senha = senha*10 + (rand() % c + 1);
+        }
+    }
+    CNpalpites= Npalpites;
 
-    while((Npalpites !=0)&&(pinos!=k)){  // o looping s� deve terminar quando os palpites acabarem ou a senha for acertada, ou seja, os pinos pretos for igual ao numero de digitos da senha
+    while((Npalpites !=0)&&(pinos!=k)){  // o looping so deve terminar quando os palpites acabarem ou a senha for acertada, ou seja, os pinos pretos for igual ao numero de digitos da senha
     printf("Digite seu palpite: ", k);
     scanf("%d", &palpite);
     Cpalpite=palpite;
     Csenha=senha;
     pinos=0;
+
     do{
         a=Cpalpite%10;
         b=Csenha%10;
         if(a==b){
             pinos= pinos+1;
             Cpalpite=Cpalpite/10;
-            Csenha=Csenha/10;}
+            Csenha=Csenha/10;
+            }
         else{
             Cpalpite=Cpalpite/10;
             Csenha=Csenha/10;}
-        }while((a>0)||(b>0));  //enquanto o numero n�o for totalmente descascado o loopin ir� continuar.
+        }while((a>0)||(b>0));  //enquanto o numero nao for totalmente descascado o loopin ir� continuar.
     pinos= pinos-1;
     printf("%d pretos \n", pinos);
     Npalpites= Npalpites-1;
-    cont=cont+1;}
+    cont=cont+1;
+    }
+
     if(pinos==k){
         printf("Parabens! Voce descobriu a senha em %d tentativas", cont);}
     else{
